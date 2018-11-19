3d1f2c9a6304e193a0f5b7972b6cc3298a062b46
dekc89 <dekc89@gmail.com>
2018-11-19 19:34:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,17 @@ const currentLine = (cQueue) => {
   if (cQueue.length === 0) {
     return "The line is currently empty.";
   }
+<<<<<<< HEAD
   let finalString = "The line is currently:";
   for (let i = 0; i < cQueue.length; i++) {
     finalString += ` ${i+1}. ${cQueue[i]},`;
   }
   finalString = finalString.slice(0, -1);
+=======
+  let finalString = "";
+  for (let i = 0; i < cQueue.length; i++) {
+    finalString += `${i+1}. ${cQueue[i]}.`;
+  }
+>>>>>>> 7295d3b2c22e51a27f428aec4da6af627eafbf80
   return finalString;
 }
